839c656a31e7f2fea3a2a249440545f1ac380705
sad0p <sad0p@protonmail.com>
2020-12-03 00:27:21 -0500

    implementing s3push --reading que dir and associating tracker with files

--- s3push.py
+++ s3push.py
@@ -1,2 +1,22 @@
 import boto3
+import json
+import dirmonitor as dirmon
 
+
+def get_que_list(que_dir):
+    que = dict()
+    que_list = dirmon.gen_file_list(que_dir)
+    for item in que_list:
+        que[item] = get_file_list(item)
+    return que
+
+
+def get_file_list(que_file):
+    file_list = []
+    with open(que_file, 'r') as fh:
+        file_list = json.load(fh)
+    return file_list
+
+
+if __name__ == '__main__':
+    print(get_que_list('/home/sad0p/.s3sync/ques'))
