878db43546a9f406c2afc94d42cf6b5e465ed6dc
sad0p <sad0p@protonmail.com>
2020-12-04 10:32:48 -0500

    added bucketname option to config.py/added code to retrieve name from config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 import stat
-
+import s3hash
 
 class ParseConfig:
     def __init__(self, config_file):
         self.root_dir = None
         self.db_location = None
+        self.bucket_name = None
+
         with open(config_file, 'r') as fh:
             for line in fh:
                 if line[0] != '#' and line[0] != '\n':
@@ -15,6 +17,8 @@ class ParseConfig:
                         self.root_dir = value
                     if variable == 'DB_LOCATION':
                         self.db_location = value
+                    if variable == 'BUCKET_NAME':
+                        self.bucket_name = value
 
         if self.root_dir is None:
             sys.exit(f"Error: config file -> {config_file} no ROOT_DIR")
@@ -22,6 +26,9 @@ class ParseConfig:
         if self.db_location is None:
             sys.exit(f"Error: config file -> {config_file} no DB_LOCATION")
 
+        if self.bucket_name is None or self.bucket_name == '':
+            self.user =  os.getenv("USER")
+            self.bucket_name = f's3sync-{s3hash.encode_path(self.user)}'
         self.trackers_dir = os.path.join(self.db_location, 'trackers')
 
     def in_scope(self, target_dir):
@@ -35,15 +42,16 @@ class ParseConfig:
 
 
 def write_config(s3sync_config, home_dir):
-    default_config = '#ROOT_DIR -  This field ensure s3sync can\'t \
+    default_config = '#ROOT_DIR -  This field ensures s3sync can\'t \
         perform backup actions above a giving directory.\n' \
         '#SET to \'/\' (without quotes) for system wide backup privs.\n\n'\
         f'ROOT_DIR={home_dir}\n\n' \
         '#s3sync will need a location to store file state information to' \
         'to detect changes in order to initiate uploads.\n' \
-        '#The variable \'DB_LOCATION\' specifies the directory where this \
-        information will be stored.\n\n' \
-        f'DB_LOCATION={home_dir}/.s3sync\n'
+        '#The variable \'DB_LOCATION\' specifies the directory where this ' \
+        'information will be stored.\n\n' \
+        f'DB_LOCATION={home_dir}/.s3sync\n' \
+        'BUCKET_NAME=\n'
 
     with open(s3sync_config, 'w') as fh:
         fh.write(default_config)
--- s3push.py
+++ s3push.py
@@ -1,5 +1,8 @@
-import boto3
+import os
 import json
+import boto3
+import s3hash
+import config
 import dirmonitor as dirmon
 
 
@@ -7,6 +10,7 @@ def get_que_list(que_dir):
     que = dict()
     que_list = dirmon.gen_file_list(que_dir)
     for item in que_list:
+        # s3hash.decode_path(os.path.basename(item))
         que[item] = get_file_list(item)
     return que
 
@@ -18,5 +22,9 @@ def get_file_list(que_file):
     return file_list
 
 
+def get_bucket_name():
+    config_obj = config.ParseConfig('/home/sad0p/.s3sync/s3sync.config')
+    return config_obj.bucket_name
+
 if __name__ == '__main__':
-    print(get_que_list('/home/sad0p/.s3sync/ques'))
+    print(get_bucket_name())
