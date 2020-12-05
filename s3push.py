99a0645415cbc7448a041b93fd1ade23b94cdfa8
sad0p <sad0p@protonmail.com>
2020-12-04 23:25:36 -0500

    implemented feeding qued files into upload func via boto3

--- s3push.py
+++ s3push.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import json
 import boto3
+import binascii
 from botocore.exceptions import ClientError
 import s3hash
 import config
@@ -27,6 +29,7 @@ def get_bucket_name():
     config_obj = config.ParseConfig('/home/sad0p/.s3sync/s3sync.config')
     return config_obj.bucket_name
 
+
 def bucket_exist(bucket_name, s3_object):
     bucket_list = s3_object.list_buckets()['Buckets']
     for bucket in bucket_list:
@@ -34,23 +37,31 @@ def bucket_exist(bucket_name, s3_object):
             return True
     return False
 
+
 def upload(bucket_name, file_path, s3_object):
     if bucket_exist(bucket_name, s3_object) is False:
         make_bucket(bucket_name, s3_object)
-    
+
     file_name = os.path.basename(file_path)
     try:
         response = s3_object.upload_file(file_path, bucket_name, file_name)
     except ClientError:
         print(f"Failed to upload to {file_path} to {bucket_name}")
-    print(response)
+
 
 def make_bucket(bucket_name, s3_object):
-        try:
-            s3_object.create_bucket(Bucket=bucket_name)
-        except ClientError:
-            print(f"Error creating bucket {bucket_name}")
+    try:
+        s3_object.create_bucket(Bucket=bucket_name)
+    except ClientError:
+        sys.exit(f"Error creating bucket {bucket_name}")
+
 
 if __name__ == '__main__':
     s3_object = boto3.client('s3')
-    upload('blah-sad0p','/tmp/tesfile', s3_object)
+    ques = get_que_list('/home/sad0p/.s3sync/ques')
+    for item in ques:
+        bucket_name = (s3hash.decode_path(
+                        os.path.basename(item)).replace('/', '-')[1:].replace('_', '.'))
+        file_list = ques[item]
+        for f in file_list:
+            upload(bucket_name, f, s3_object)
